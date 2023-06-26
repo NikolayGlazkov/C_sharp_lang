bb283846255006ade1594ae9549ecbc48adda5df
NikolayGlazkov <gn9377419582@gmail.com>
2023-06-26 21:56:29 +0300

    gitignore made

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("введите имя! ");
+string username = Console.ReadLine();
+Console.Write("привет, ");
+Console.Write(username);
+
+
