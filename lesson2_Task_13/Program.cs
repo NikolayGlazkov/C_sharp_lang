6f35443a147e9f3736cd526052655f8d69847edf
NikolayGlazkov <gn9377419582@gmail.com>
2023-06-27 17:00:50 +0300

    lesson_2 Task_13_Ended

---
+++ lesson2_Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string strnum = num.ToString();
+
+
+if (strnum.Length < 3)
+{
+    Console.WriteLine("Длинна числа меньше 3");
+}
+else
+{
+    Console.WriteLine($"третее число: {strnum[2]}");
+}
