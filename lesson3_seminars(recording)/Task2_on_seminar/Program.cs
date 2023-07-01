bac9e0e33eda0e23369164e377d807458abeebe5
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-01 21:55:56 +0300

    made some changes

--- lesson3_seminars(recording)/Task2_on_seminar/Program.cs
+++ lesson3_seminars(recording)/Task2_on_seminar/Program.cs
@@ -3,39 +3,31 @@
 возможных координат точек в этой четверти (x и y).
 */
 
-using System;
-
-class Program
+static string FindQuarter(int num)
 {
-    static string FindQuarter(int num)
-    {
-        string answer = "";
-        
-        if (num == 1)
-        {
-            answer = "Диапазон оси X: от 0 до +X, диапазон оси Y: от 0 до +Y";
-        }
-        else if (num == 2)
-        {
-            answer = "Диапазон оси X: от -X до 0, диапазон оси Y: от -Y до 0";
-        }
-        else if (num == 3)
-        {
-            answer = "Диапазон оси X: от -X до 0, диапазон оси Y: от 0 до +Y";
-        }
-        else if (num == 4)
-        {
-            answer = "Диапазон оси X: от 0 до +X, диапазон оси Y: от -Y до 0";
-        }
-        
-        return answer;
+    string answer = "";
+    if (num == 1)
+    {            
+        answer = "x > 0 and y > 0";
     }
-
-    static void Main()
+    else if (num == 2)
     {
-        Console.Write("Введите четверть от 1 до 4: ");
-        int digit = int.Parse(Console.ReadLine());
-
-        Console.WriteLine(FindQuarter(digit));
+        answer = "x < 0 and y > 0";
     }
+    else if (num == 3)
+    {
+        answer = "x < 0 adn y < 0";
+    }
+    else if (num == 4)
+    {
+        answer = "x > 0 and y < 0";
+    }
+        
+    return answer;
 }
+
+    Console.Write("Введите четверть от 1 до 4: ");
+    int digit = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(FindQuarter(digit));
+
