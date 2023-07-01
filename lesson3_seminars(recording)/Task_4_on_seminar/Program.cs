b9039b4ca5a80604854378880a2c0ea1f87419b0
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-01 23:27:32 +0300

    finished task №4

---
+++ lesson3_seminars(recording)/Task_4_on_seminar/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу которая получает на входе
+ число N и выводит таблицу квадратов до числа N.*/
+
+
+static void GenerateSquareTable(int N)
+{
+for (int i = 0; i <= N; i++)
+{
+       Console.WriteLine($"Квадрат числа {i}: {Math.Pow(i, 2)}");
+}
+}
+Console.WriteLine("Введите число N:");
+int num = Convert.ToInt32(Console.ReadLine());
+GenerateSquareTable(num);
+    
+
+
