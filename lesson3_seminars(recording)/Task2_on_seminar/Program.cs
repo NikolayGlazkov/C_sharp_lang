46bda6c723e8e7a153fe9b0d6bf02172f6c1c39c
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-01 21:40:12 +0300

    I finished task 2 on my own now I'm looking at the solution in the post in the group

--- lesson3_seminars(recording)/Task2_on_seminar/Program.cs
+++ lesson3_seminars(recording)/Task2_on_seminar/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите задачу, которая по заданнуму номеру четверти, показывает диапазон 
+возможных координат точек в этой четверти (x и y).
+*/
+
+using System;
+
+class Program
+{
+    static string FindQuarter(int num)
+    {
+        string answer = "";
+        
+        if (num == 1)
+        {
+            answer = "Диапазон оси X: от 0 до +X, диапазон оси Y: от 0 до +Y";
+        }
+        else if (num == 2)
+        {
+            answer = "Диапазон оси X: от -X до 0, диапазон оси Y: от -Y до 0";
+        }
+        else if (num == 3)
+        {
+            answer = "Диапазон оси X: от -X до 0, диапазон оси Y: от 0 до +Y";
+        }
+        else if (num == 4)
+        {
+            answer = "Диапазон оси X: от 0 до +X, диапазон оси Y: от -Y до 0";
+        }
+        
+        return answer;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите четверть от 1 до 4: ");
+        int digit = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(FindQuarter(digit));
+    }
+}
