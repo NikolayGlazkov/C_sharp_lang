abdd5dd42ea06c0dafeddace22bf6e8ab4f97601
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-01 23:10:29 +0300

    contribution to the solution of the third task

--- lesson3_seminars(recording)/Task_3_on_seminar/Program.cs
+++ lesson3_seminars(recording)/Task_3_on_seminar/Program.cs
@@ -15,14 +15,14 @@ class Program
     static void Main()
     {
         Console.WriteLine("Введите координаты первой точки (x1, y1):");
-        int x1 = Convert.ToInt32(Console.ReadLine());
-        int y1 = Convert.ToInt32(Console.ReadLine());
+        int xcord1 = Convert.ToInt32(Console.ReadLine());
+        int ycord1 = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Введите координаты второй точки (x2, y2):");
-        int x2 = Convert.ToInt32(Console.ReadLine());
-        int y2 = Convert.ToInt32(Console.ReadLine());
+        int xcord2 = Convert.ToInt32(Console.ReadLine());
+        int ycord2 = Convert.ToInt32(Console.ReadLine());
 
-        double distance = DistanceBetweenPoints(x1, y1, x2, y2);
-        Console.WriteLine("Расстояние между точками: " + distance);
+        double answer = DistanceBetweenPoints(xcord1, ycord1, xcord2, ycord2);
+        Console.WriteLine($"Расстояние между точками: {answer}");
     }
 }
