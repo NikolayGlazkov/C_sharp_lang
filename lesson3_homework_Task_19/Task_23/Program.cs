df3cb8e1f420d80e9c36522b95d11ed52578ab97
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-02 15:22:48 +0300

    Task 23 Ended

---
+++ lesson3_homework_Task_19/Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу котрая принимает на вфод число N и выводиит кубы от 1 до N
+*/ 
+static void GenerateSquareTable(int N)
+{
+for (int i = 1; i <= N; i++)
+{
+       Console.WriteLine($"Куб числа {i}: {Math.Pow(i, 3)}");
+}
+}
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+GenerateSquareTable(num);
