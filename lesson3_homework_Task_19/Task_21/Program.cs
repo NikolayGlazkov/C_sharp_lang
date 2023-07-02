d8ff0384f37e94e9aaee327cb78f0a499f62319e
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-02 15:17:23 +0300

    Task 21 in my home work Ended

---
+++ lesson3_homework_Task_19/Task_21/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу которая принимает на вход кординаты двух точек и находит 
+между ними растояние в 3D пространстве
+*/
+using System;
+
+class Program
+{
+    static double DistanceBetweenPoints(int x1, int y1, int z1, int x2, int y2, int z2)
+    {
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2));
+        return distance;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите координаты первой точки (x1, y1, z1):");
+        int xcord1 = Convert.ToInt32(Console.ReadLine());
+        int ycord1 = Convert.ToInt32(Console.ReadLine());
+        int zcord1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координаты второй точки (x2, y2, z2):");
+        int xcord2 = Convert.ToInt32(Console.ReadLine());
+        int ycord2 = Convert.ToInt32(Console.ReadLine());
+        int zcord2 = Convert.ToInt32(Console.ReadLine());
+
+        double answer = DistanceBetweenPoints(xcord1, ycord1, zcord1, xcord2, ycord2, zcord2);
+        Console.WriteLine($"Расстояние между точками: {answer}");
+    }
+}
