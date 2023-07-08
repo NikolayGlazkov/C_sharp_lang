a1ec80b95b8e5a8a8bfda5cd848be9d4b39b32aa
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-08 21:20:18 +0300

     Task 38 Ended

---
+++ Task_38/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите 
+разницу между максимальным и минимальным элементов массива.
+*/
+
+
+float[] arr = new float[10];
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0, 999);
+}
+float minDigit = arr[0];
+float maxDigit = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (minDigit > arr[i])
+{
+    minDigit = arr[i];
+}
+    if (maxDigit < arr[i])
+{
+    maxDigit = arr[i];
+}
+}
+
+float difference = maxDigit - minDigit;
+
+Console.WriteLine("Массив: " + string.Join(", ", arr));
+Console.WriteLine("Большее число в массиве: " + maxDigit);
+Console.WriteLine("Меньшее число в массиве: " + minDigit);
+Console.WriteLine("Разница между максимальным и минимальным элементами: " + difference);
