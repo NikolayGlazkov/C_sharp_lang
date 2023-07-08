63fd1e98491285bced7b8e11c4fb6abd98a9dda9
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-08 16:08:58 +0300

    Task 34 ended

---
+++ Task_34/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] arr = new int[4];
+Random rand = new Random();
+int counter = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+   arr[i] = rand.Next(99,999); // Заполняем случайными числами диапазоном от -100 до 100
+}
+
+foreach (int num in arr)
+{
+if (num % 2 == 0)
+{
+   counter++;
+}
+}
+
+        Console.WriteLine("Массив: " + string.Join(", ", arr));
+        Console.WriteLine("Количество четных элементов в массиве: " + counter);
