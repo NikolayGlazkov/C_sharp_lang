e7a813059c5b14787b02c45880764894e2a72c40
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-10 17:45:08 +0300

    fix

---
+++ Task_42/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая будет преобразовывать десятичное число в двоичное.*/ 
+
+// int x = 32;
+// string bin = Convert.ToString(x,2);
+// Console.WriteLine(bin);
+
+
+Console.WriteLine("Введите число :");
+
+int digit = Convert.ToInt32(Console.ReadLine());
