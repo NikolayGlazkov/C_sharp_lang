3b47260dfc8b6b337d4873ea6f324e377a323ffb
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-10 14:05:04 +0300

    Task_40 Ended

---
+++ Task_40/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины.*/
+
+Console.WriteLine("Ведите 3 отдельных числа чила:");
+Console.Write("Ведите 1 чило:");
+int dot1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите 2 чило:");
+int dot2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите 3 чило:");
+int dot3 = Convert.ToInt32(Console.ReadLine());
+
+if (dot1 + dot2 > dot3 && dot3 + dot1 > dot2 && dot2 + dot3 > dot1)
+{
+    Console.WriteLine("Все ок:");
+}
+else
+{
+    Console.WriteLine("Все не ок:");
+}
