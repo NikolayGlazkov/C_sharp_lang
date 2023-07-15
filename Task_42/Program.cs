13abafd549893e3ca047164a270af36aea334cc1
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-15 23:22:05 +0300

    i was made task from lesson

--- Task_42/Program.cs
+++ Task_42/Program.cs
@@ -1,12 +1,19 @@
-﻿/*Напишите программу, которая будет преобразовывать десятичное число в двоичное.*/ 
+﻿/*Напишите программу, которая будет преобразовывать десятичное
+ число в двоичное.*/ 
 
 // int x = 32;
 // string bin = Convert.ToString(x,2);
 // Console.WriteLine(bin);
 
+static string  ToBinInt(double dec)
+    {
+        string str = "";
+        while (dec > 0)
+        {
+            str =String.Concat(Convert.ToString(dec%2),str);
+            dec = Math.Truncate(dec/2);
+        }
+        return str;
+    }
 
-Console.WriteLine("Введите число :");
-
-int digit = Convert.ToInt32(Console.ReadLine());
-
-string
+Console.WriteLine(ToBinInt(32));
--- Task_45/Program.cs
+++ Task_45/Program.cs
@@ -3,7 +3,9 @@
 
 
 
-int CopyArray(int [] array)
-{
-    for  
-}
+int[] arr = { 5, 4, 7, 2, 9 };
+ 
+int[] copy = new int[arr.Length];
+arr.CopyTo(copy, 0);
+ 
+Console.WriteLine(String.Join(", ", copy));
