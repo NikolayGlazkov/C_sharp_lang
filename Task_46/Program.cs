898a047c27a84b1136662fca073a14449cb27879
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-20 00:26:33 +0300

    TasK_46

--- Task_46/Program.cs
+++ Task_46/Program.cs
@@ -1,22 +1,37 @@
 ﻿/*Задайте двумерный массив размера m*n, заполненый члучайными числами.*/
 
 int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
-{
-    int[,] resukt = new int[rowNumber,colNumber];
-    for (int i = 0; i < rowNumber; i++)
     {
-        for (int j = 0; j < colNumber; j++)
+        int[,] result = new int[rowNumber, colNumber];
+        Random random = new Random();
+
+        for (int i = 0; i < rowNumber; i++)
         {
-           result[i,j] = new Random().Next(-deviation,deviation+1) 
+            for (int j = 0; j < colNumber; j++)
+            {
+                result[i, j] = random.Next(-deviation, deviation + 1);
+            }
+        }
+        return result;
+    }
+
+void Print2dArray(int[,] arrayToPrint)
+    {
+        for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+        {
+            for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+            {
+                Console.Write(arrayToPrint[i, j] + "\t");
+            }
+            Console.WriteLine();
         }
     }
-    return result;
-}
+Console.Write("Введите ширину :");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите высоту :");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число :");
+int div = Convert.ToInt32(Console.ReadLine());
 
-void Print2dArray(int[,] arrayToprint)
-{
-    for (int i = 0; i < arrayToprint.GetLenght(0); i++)
-    {
-        
-    }
-}
+int[,] randomArray = GetRandom2DArray(row, col, div);
+Print2dArray(randomArray);
