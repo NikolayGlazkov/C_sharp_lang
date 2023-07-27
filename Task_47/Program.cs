085882e9145b467bf7746823965d4a27c4274e1b
NikolayGlazkov <gn9377419582@gmail.com>
2023-07-27 11:23:35 +0300

    fix

---
+++ Task_47/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/
+
+
+int m = 3; // Количество строк
+int n = 4; // Количество столбцов
+
+
+Random random = new Random();
+
+// Создайте двумерный массив
+double[,] array = new double[m, n];
+double minValue = 0.0;
+double maxValue = 100.0;
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        // Генерируем случайное вещественное число от мин до мах
+        double randomValue = random.NextDouble() * (maxValue - minValue) + minValue;
+        array[i, j] = randomValue;
+    }
+}
+
+
+Console.WriteLine("Случайно заполненный массив:");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
---
+++ Task_50/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.*/
+
+
+Console.Write("Введите Количество строк: ");
+int m;
+while (!int.TryParse(Console.ReadLine(), out m))
+{
+    Console.WriteLine("Некорректный ввод. Введите целое число для Количество строк:");
+}
+
+Console.Write("Введите Количество столбцов: ");
+int n;
+while (!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine("Некорректный ввод. Введите целое число для Количество столбцов:");
+}
+
+double[,] array = new double[m, n];
+double minValue = 0.0;
+double maxValue = 100.0;
+
+Random random = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        double randomValue = random.NextDouble() * (maxValue - minValue) + minValue;
+        array[i, j] = randomValue;
+    }
+}
+
+Console.Write("Введите номер строки: ");
+int row;
+while (!int.TryParse(Console.ReadLine(), out row) || row < 0 || row >= m)
+{
+    Console.WriteLine($"Некорректный ввод. Введите целое число от 0 до {m - 1} для номера строки:");
+}
+
+Console.Write("Введите номер столбца: ");
+int col;
+while (!int.TryParse(Console.ReadLine(), out col) || col < 0 || col >= n)
+{
+    Console.WriteLine($"Некорректный ввод. Введите целое число от 0 до {n - 1} для номера столбца:");
+}
+
+double value = array[row, col];
+Console.WriteLine($"Значение элемента [{row}, {col}]: {value}");
